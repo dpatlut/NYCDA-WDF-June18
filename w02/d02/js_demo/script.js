07339d9ee5cbfa832154e88a62921af0c8bd22ed
dpatlut <dpatlut@gmail.com>
2018-06-26 16:49:38 -0400

    reading

---
+++ w02/d02/js_demo/script.js
@@ -0,0 +1,127 @@
+// let myGrade = 90.99;
+
+// const myName = "David";
+
+// let awesomeClass = true;
+
+// awesomeClass = false;
+
+// let myAge = 90;
+
+// myName + myAge;
+
+
+// let myBody = {
+// 	name: "David",
+// 	eyeColor: "Blue"
+// }
+
+// myBody
+// {name: "David", eyeColor: "Blue"}
+
+// myBody.hairColor = "blonde"
+// "blonde"
+
+// myBody
+// {name: "David", eyeColor: "Blue", hairColor: "blonde"}
+
+
+// let myTopMovies = {
+
+// 	topMovieOne: "Walle",
+// 	topMovieTwo: "Titanic",
+// 	topMovieThree: "Pulp Fiction"
+
+
+
+
+// } 
+
+
+// myTopMovies
+// {topMovieOne: "Walle", topMovieTwo: "Titanic", topMovieThree: "Pulp Fiction"}
+// console.log("My favorite movies are" + myTopMovies.topMovieOne + "and" + myTopMovies.topMovieTwo + ", but I really enjoy" + myTopMovies.topMovieThree); 
+// raven.js:58 My favorite movies areWalleandTitanic, but I really enjoyPulp Fiction
+
+
+// console.log("My favorite movies are " + myTopMovies.topMovieOne + " and " + myTopMovies.topMovieTwo + " , but I really enjoy " + myTopMovies.topMovieThree); 
+// raven.js:58 My favorite movies are Walle and Titanic , but I really enjoy Pulp Fiction
+
+
+
+// let number = 100;
+
+// if(number>10){
+//      console.log('HELLO WORLD');
+// }
+
+// if ((number>10) && (number<200)){
+//     console.log('Execute the code in here');
+// }
+
+// if(number < 3) {
+//     // will not run code written here
+//     let thiswillnotrun = "Whatever";
+// } else if(number > 5) {
+//     // will  run code written here
+//     let thiswillrun = "Whatever";
+// }
+
+
+// let myName = "david"
+
+// if(myName === "George") {
+//     // will not run code written here
+//     let thiswillnotrun = "Whatever";
+// } else if(myName === "Josh") {
+//     // will  run code written here
+//     let thiswillnotrun = "Whatever";
+// } else if(myName === "John") {
+//     // will  run code written here
+//     let thiswillnotrun = "Whatever";
+// }else if(myName === "David") {
+//     // will  run code written here
+//     let thiswillrun = "Whatever";
+// }else {
+//      let thisismyfallback = "Whatever";
+// }
+
+
+// switch ("david") {
+//     case 0:
+//         day = "Sunday";
+//         break;
+//     case 1:
+//         day = "Monday";
+//         break;
+//     case 2:
+//         day = "Tuesday";
+//         break;
+//     case 3:
+//         day = "Wednesday";
+//         break;
+//     case 4:
+//         day = "Thursday";
+//         break;
+//     case 5:
+//         day = "Friday";
+//         break;
+//     case "David":
+//         day = "Saturday";
+// }
+
+// let array = ["David", "Josh", "John", "kevin"]
+
+// for(let i = 0; i < array.length; i++) {
+//   // displays the current value within array
+//   console.log(array[i]) // "david", "Josh", "Kevin"....
+// }
+
+// let myArr = ["David", "1" , "2"]
+// undefined
+// for( let i in myArr ) {
+// 	console.log(myArr[i])
+// }
+
+let myName = "David";
+console.log(myName);
---
+++ w02/d02/reading.md
@@ -0,0 +1,17 @@
+#Reading
+
+Functions Basics: http://www.javascripttutorial.net/javascript-function/
+
+Arrow Functions: 
+https://www.sitepoint.com/es6-arrow-functions-new-fat-concise-syntax-javascript/
+
+Array Methods:https://www.w3schools.com/js/js_array_methods.asp
+
+Scope: https://scotch.io/tutorials/understanding-scope-in-javascript
+http://2ality.com/2015/02/es6-scoping.html
+
+Callback functions:
+https://codeburst.io/javascript-what-the-heck-is-a-callback-aba4da2deced
+
+Closures:
+https://medium.com/dailyjs/i-never-understood-javascript-closures-9663703368e8
