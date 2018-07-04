b95215d5eec6c5642b9f8502d9b656ec9f435549
dpatlut <dpatlut@gmail.com>
2018-07-04 12:11:26 -0400

    workshop fix

--- w03/d02/workshop_solution/script.js
+++ w03/d02/workshop_solution/script.js
@@ -6,7 +6,7 @@ let thirdSection = sections[2]
 let lastSection = sections[3]
 
 // run this code on window scroll
-window.addEventListener("scroll", event => {
+window.addEventListener("scroll", (event) => {
   // when the page scrolls 100px down then do the following
   if(window.scrollY > 100) {
     firstSection.classList.add("fadeIn")
