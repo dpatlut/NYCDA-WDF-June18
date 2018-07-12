df4c208470011126ae486ee8465ff3d600334044
dpatlut <dpatlut@gmail.com>
2018-07-12 13:31:38 -0400

    update fix

--- w04/d04/Ajax_demo_code/script.js
+++ w04/d04/Ajax_demo_code/script.js
@@ -8,9 +8,9 @@
 
 
 //Axios Example - Wont work until you put a url 
-axios.get("SOME URL HERE").then((response) => {
-  console.log(response)
-})
+// axios.get("SOME URL HERE").then((response) => {
+//   console.log(response)
+// })
 
 //Example data we will be working with:
 //JSON Objects - Just like regular objects
@@ -24,41 +24,41 @@ axios.get("SOME URL HERE").then((response) => {
 // }
 
 //URLS with JSON data
-//Person one: https://jsonbin.io/5b478839a5a2f34ea6b4867e
+//Person one: https://api.jsonbin.io/b/5b478839a5a2f34ea6b4867e
 //Person two: https://jsonbin.io/5b47885fdd2c022ecda1e7d3 
 //All people: https://jsonbin.io/5b47888c4d5ea95c8ba39f13 
 
 
 //Example call for this data
-// axios.get("https://raw.githubusercontent.com/orlandocaraballo/sei/master/outlines/17-working-with-ajax/json/person-1.json").then((response) => {
-//     console.log(response)
-//   })
+axios.get("https://api.jsonbin.io/b/5b478839a5a2f34ea6b4867e").then((response) => {
+    console.log(response)
+  })
 
 
 //Combining our knowledge of OOP with our ajax calls
 
 // individual class person
-class Person {
-    constructor(firstName, lastName, age, gender) {
-      this.firstName = firstName
-      this.lastName = lastName
-      this.age = age
-      this.gender = gender
-    }
-  }
+// class Person {
+//     constructor(firstName, lastName, age, gender) {
+//       this.firstName = firstName
+//       this.lastName = lastName
+//       this.age = age
+//       this.gender = gender
+//     }
+//   }
 
-  //Create new person from our AJAX call
-  axios.get("https://jsonbin.io/5b478839a5a2f34ea6b4867e").then((response) => {
-   let data  = response.data 
-   let david = new Person(
-        data["first-name"],
-        data["last-name"],
-        data["age"],
-        data["gender"]
-    )
+//   //Create new person from our AJAX call
+//   axios.get("https://jsonbin.io/5b478839a5a2f34ea6b4867e").then((response) => {
+//    let data  = response.data 
+//    let david = new Person(
+//         data["first-name"],
+//         data["last-name"],
+//         data["age"],
+//         data["gender"]
+//     )
 
-    //Add david to the dom
-    let h1 = document.createElement("h1");
-    h1.innerText = david.name
-    document.body.appendChild(h1);
-  })
+//     //Add david to the dom
+//     let h1 = document.createElement("h1");
+//     h1.innerText = david.name
+//     document.body.appendChild(h1);
+//   })
