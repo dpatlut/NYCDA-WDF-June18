ec31a5741653f19673198c5752317da45bb0e3ef
dpatlut <dpatlut@gmail.com>
2018-08-01 12:54:45 -0400

    sllfsdf

--- w07-Sinatra/d03/sinatra_demo/app.rb
+++ w07-Sinatra/d03/sinatra_demo/app.rb
@@ -1,22 +1,23 @@
 require 'sinatra'
+require 'sinatra/reloader'
 require './turtle.rb'
-# require './sinatra/reloader'
 
 get '/' do
+    #Cant access this information from client side
     y = 10
     x = 5
     "<a href='/turtle'>Go to my turtle page</a>"
 end
 
 get '/turtle/:name' do 
-    puts "LOOK DOWN VVVV"
+    puts "LOOK DOWN HERE VVVV"
     puts params[:name]
-    puts "NAME ^^^^"
+    puts "PARAMS NAME ^^^^"
 
     turtle_name = params[:name]
 
     "Hello #{turtle_name}"
-    
+
     # if turtle_name == "bobby"
     #     "Hello Bobby"
     # elsif turtle_name == "david"
@@ -26,4 +27,12 @@ get '/turtle/:name' do
     # end
     # bob = Turtle.new('bobby', 15)
     # "#{bob.name}"
+end
+
+get '/test' do 
+    puts_stuff
+end
+
+def puts_stuff 
+    puts "hello world"
 end
