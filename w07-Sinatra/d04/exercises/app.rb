2385ee5c0cb1c2902f42e37d4fa0e03a67da805d
dpatlut <dpatlut@gmail.com>
2018-08-02 11:59:35 -0400

    slkdjasd

---
+++ w07-Sinatra/d04/exercises/app.rb
@@ -0,0 +1,12 @@
+require 'sinatra'
+
+get '/' do 
+    erb :form
+end
+
+get '/query' do 
+    #capture the params from our submitted form
+    @username = "Persons Username: " + params[:username] 
+    @id = params[:id].to_i * 1000
+    erb :results
+end
