4d27c06b0d5aef51d111e9f7b509c6ebd52e6d2b
dpatlut <dpatlut@gmail.com>
2018-08-10 11:54:05 -0400

    sdsd

--- w08/d05/workshop_solution/app.rb
+++ w08/d05/workshop_solution/app.rb
@@ -2,8 +2,11 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require './models/dog.rb'
 require './models/owner.rb'
+require 'giphy'
 
-
+Giphy::Configuration.configure do |config|
+    config.api_key = ENV['API_GIPHY']
+end
 
 set :database, {adapter: "postgresql", database: "dog_owners"}
 
@@ -22,6 +25,13 @@ get '/owners' do
     erb :owners
 end
 
+get '/randomdoggie' do
+    random_dog_name = ['gin', 'vodka', 'tonic']
+    random_breed = ['corgie', 'pup'] 
+    Dog.create(name: random_dog_name.sample, breed: random_breed.sample, age: 20)
+    redirect '/dogs'
+end
+
 
 get '/owners/:id' do 
     #show all dogs that belong to a specific owner
@@ -30,4 +40,8 @@ get '/owners/:id' do
     erb :specific_owner
 end
 
-
+get '/woof' do 
+    # @gif = Giphy.search('funny dog', {limit: 1})
+    @gif = Giphy.random('dog')
+    erb :woof
+end
