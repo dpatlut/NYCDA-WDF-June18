3ce2053fe4c6478543238ac01f2ebb51e4e815b3
dpatlut <dpatlut@gmail.com>
2018-08-10 11:05:50 -0400

    dfdfd

--- w08/d05/workshop_solution/app.rb
+++ w08/d05/workshop_solution/app.rb
@@ -11,3 +11,23 @@ set :database, {adapter: "postgresql", database: "dog_owners"}
 get '/' do 
     erb :homepage
 end
+
+get '/dogs' do 
+    @dogs = Dog.all
+    erb :dogs
+end 
+
+get '/owners' do 
+    @owners = Owner.all
+    erb :owners
+end
+
+
+get '/owners/:id' do 
+    #show all dogs that belong to a specific owner
+    @specific_owner = Owner.find(params[:id])
+    @owners_dogs = @specific_owner.dogs
+    erb :specific_owner
+end
+
+
